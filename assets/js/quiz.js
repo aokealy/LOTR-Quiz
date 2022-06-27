8494f4c3b07d271edcd50a3d1d7567c7010dbae8
aokealy <aokealy@gmail.com>
2022-06-27 22:38:51 +0000

    add grid to text and add buttons in questions.html file

---
+++ assets/js/quiz.js
@@ -0,0 +1,85 @@
+let quiz = [
+    {
+        question: "Who directed Lord Of The Rings?",
+        option: [
+            "Peter Jackson",
+            "Steven Spielberg",
+            "James Cameron",
+            "Christopher Nolan",
+        ],
+        answer: 1,
+    },
+    {
+        question: "When was the Fellowship of the released?",
+        option: [
+            "2004",
+            "2005",
+            "2001",
+            "2000",
+        ],
+        answer: 3,
+    },
+    {
+        question: "Who race is Frodo Baggins?",
+        option: [
+            "Elve",
+            "Dwarve",
+            "Man",
+            "Hobbit",
+        ],
+        answer: 4,
+    },
+    {
+        question:
+            "What is the correct format for aligning written content to the center of the page in CSS?",
+        option: [
+            "Text-align:center;",
+            "Font-align:center;",
+            "Text:align-center;",
+            "Font:align-center;",
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            "What is the correct format for changing the background colour of a div in CSS?",
+        option: [
+            "Bg-color:red;",
+            "bg:red;",
+            "Background-colour:red;",
+            "Background-color:red;",
+        ],
+        answer: 4,
+    },
+    {
+        question: "Choose the correct HTML tag for the largest heading",
+        option: ["<heading>", "<h6>", "<head>", "<h1>"],
+        answer: 4,
+    },
+    {
+        question: "Which is the correct CSS syntax?",
+        option: [
+            "Body {color: black}",
+            "{body;color:black}",
+            "{body:color=black(body}",
+            "body:color=black",
+        ],
+        answer: 1,
+    },
+    {
+        question:
+            "In CSS, what is the correct option to select all the tags on a page?",
+        option: ["<p> { }", ".p { }", "#p { }", "* { }"],
+        answer: 4,
+    },
+    {
+        question: "Select the correct HTML tag to make a text italic?",
+        option: ["Italic", "II", "IT", "I"],
+        answer: 4,
+    },
+    {
+        question: "Select the correct HTML tag to make a text bold.",
+        option: ["bo", "bb", "b", "bold"],
+        answer: 3,
+    },
+];
