fa327bd493ee52c9e6560c31e50d33d48192537e
aokealy <aokealy@gmail.com>
2022-06-28 09:36:40 +0000

    add timer function in script.js for timer

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+$(function () {
+
+    //timer for code starts here
+    let totalTime = 200; //200 seconds for the timer 
+    let min = 0;
+    let sec = 0;
+
+    let timer = setInterval(function(){
+
+    }, 1000); // timer set for 1 second interval
+
+    //timer for code ends here
+
+});
